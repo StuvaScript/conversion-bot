b9e3c568ee5fd6c2ca0cfde91668515331ca9bf5
Stu <stumac1000@yahoo.com>
2023-04-12 11:33:59 -0700

    Add logic Take input and convert to a reversed array. Reverse the array again, join the values into a single string, display the results on the screen.

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
+// ---- GLOBAL VARIABLES ----
 const button = document.querySelector("button");
 const input = document.querySelector("input");
 const display = document.querySelector(".display");
+let values;
 
+// ---- CONVERSION LOGIC ----
 button.addEventListener("click", (e) => {
   e.preventDefault();
-  convertNumbers(input.value);
-  //   console.log(input.value);
-  //   display.innerText = input.value;
+  console.log(input.value + " input value");
+  putMyThangDownFlipItAndReverseIt();
+  console.log("new reversed array below");
+  console.log(values);
+  anotherFlipper();
+  console.log(values + " final output");
+  display.innerText = values;
 });
+
+// ---- FUNCTIONS ----
+function anotherFlipper() {
+  // Reverses the array and joins the array values together into a single string.
+  values = values.reverse().join(" ");
+}
+
+function putMyThangDownFlipItAndReverseIt() {
+  // Creates an array of the input numbers and reverses their order.
+  values = input.value.split("").reverse();
+}
 
 function convertNumbers(value) {
   switch (value) {
