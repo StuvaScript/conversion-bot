e2811ef58ebe2bd0f1b1449bcdef89da1f1a3f59
Stu <stumac1000@yahoo.com>
2023-04-13 10:33:43 -0700

    Add a loop that calls the array functions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const button = document.querySelector("button");
 const input = document.querySelector("input");
 const display = document.querySelector(".display");
-let values;
+let valuesArray;
 
 // ---- CONVERSION LOGIC ----
 button.addEventListener("click", (e) => {
@@ -10,51 +10,136 @@ button.addEventListener("click", (e) => {
   console.log(input.value + " input value");
   putMyThangDownFlipItAndReverseIt();
   console.log("new reversed array below");
-  console.log(values);
+  console.log(valuesArray);
+
+  loopyDoop();
+
   anotherFlipper();
-  console.log(values + " final output");
-  display.innerText = values;
+  console.log(valuesArray + " final output");
+  display.innerText = valuesArray;
 });
 
 // ---- FUNCTIONS ----
+function loopyDoop() {
+  // Loops thru the array
+  for (let i = 0; i < valuesArray.length; i++) {
+    // At the different array positions, call on the correct function
+    switch (i) {
+      case 0:
+        console.log(valuesArray[i] + " zeroth");
+        singleDigits(valuesArray[i], i);
+        break;
+      case 1:
+        console.log(valuesArray[i] + " first");
+        tensPosition(valuesArray[i], i);
+        break;
+      case 2:
+        console.log(valuesArray[i] + " second");
+        singleDigits(valuesArray[i], i);
+        break;
+      case 3:
+        console.log(valuesArray[i] + " third");
+        singleDigits(valuesArray[i], i);
+        break;
+      case 4:
+        console.log(valuesArray[i] + " fourth");
+        tensPosition(valuesArray[i], i);
+        break;
+      case 5:
+        console.log(valuesArray[i] + " fifth");
+        singleDigits(valuesArray[i], i);
+        break;
+      case 6:
+        console.log(valuesArray[i] + " sixth");
+        singleDigits(valuesArray[i], i);
+        break;
+      case 7:
+        console.log(valuesArray[i] + " seventh");
+        tensPosition(valuesArray[i], i);
+        break;
+      case 8:
+        console.log(valuesArray[i] + " eighth");
+        singleDigits(valuesArray[i], i);
+        break;
+      case 9:
+        console.log(valuesArray[i] + " ninth");
+        singleDigits(valuesArray[i], i);
+        break;
+    }
+  }
+}
+
 function anotherFlipper() {
   // Reverses the array and joins the array values together into a single string.
-  values = values.reverse().join(" ");
+  valuesArray = valuesArray.reverse().join(" ");
 }
 
 function putMyThangDownFlipItAndReverseIt() {
   // Creates an array of the input numbers and reverses their order.
-  values = input.value.split("").reverse();
+  valuesArray = input.value.split("").reverse();
 }
 
-function convertNumbers(value) {
-  switch (value) {
+function singleDigits(digit, position) {
+  // 'Digit' is the input value
+  // 'Position' is the location in the array
+  switch (digit) {
     case "1":
-      display.innerText = "one";
+      valuesArray[position] = "one";
       break;
     case "2":
-      display.innerText = "two";
+      valuesArray[position] = "two";
       break;
     case "3":
-      display.innerText = "three";
+      valuesArray[position] = "three";
       break;
     case "4":
-      display.innerText = "four";
+      valuesArray[position] = "four";
       break;
     case "5":
-      display.innerText = "five";
+      valuesArray[position] = "five";
       break;
     case "6":
-      display.innerText = "six";
+      valuesArray[position] = "six";
       break;
     case "7":
-      display.innerText = "seven";
+      valuesArray[position] = "seven";
       break;
     case "8":
-      display.innerText = "eight";
+      valuesArray[position] = "eight";
       break;
     case "9":
-      display.innerText = "nine";
+      valuesArray[position] = "nine";
+      break;
+  }
+}
+
+function tensPosition(digit, position) {
+  // 'Digit' is the input value
+  // 'Position' is the location in the array
+  switch (digit) {
+    case "2":
+      valuesArray[position] = "twenty";
+      break;
+    case "3":
+      valuesArray[position] = "thirty";
+      break;
+    case "4":
+      valuesArray[position] = "forty";
+      break;
+    case "5":
+      valuesArray[position] = "fifty";
+      break;
+    case "6":
+      valuesArray[position] = "sixty";
+      break;
+    case "7":
+      valuesArray[position] = "seventy";
+      break;
+    case "8":
+      valuesArray[position] = "eighty";
+      break;
+    case "9":
+      valuesArray[position] = "ninety";
       break;
   }
 }
